695ccc4074eab45a15674bddc9514876fc819421
Space-Banane <paulwaehner923@gmail.com>
2022-08-29 19:21:03 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
-print("'Professional' Setup Done")
+from setuptools import setup
+setup(
+    name="DbPy",
+    version="1.5",
+    url="https://github.com/Space-Banane/dbpy",
+    author="Space-Banane#5444",
+    description="QSL Connection for your script"
+)
