7f42825d0ca4163e42fb42a8f0f10b387b7cae18
Space-Banane <paulwaehner923@gmail.com>
2022-08-29 19:46:40 +0200

    prep

--- setup.py
+++ setup.py
@@ -7,3 +7,31 @@ setup(
     description="QSL Connection for your script",
     requires=["pymysql"]
 )
+
+
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'QSL Connection for your script'
+
+# Setting up
+setup(
+    name="DbPy",
+    version=VERSION,
+    author="Paul",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["pymysql"],
+    keywords=["Database","qsl"],
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
