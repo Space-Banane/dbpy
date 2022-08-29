cf253dbd8ad205619f4ab05c72ce51256c94be71
Space-Banane <paulwaehner923@gmail.com>
2022-08-29 19:59:24 +0200

    fix

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-from setuptools import setup
-setup(
-    name="qslpy",
-    version="1.5",
-    url="https://github.com/Space-Banane/dbpy",
-    author="Space-Banane#5444",
-    description="QSL Connection for your script",
-    requires=["pymysql"]
-)
-
-
 from setuptools import setup, find_packages
 import codecs
 import os
